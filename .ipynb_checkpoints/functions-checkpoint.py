1b5d397fcc7b50f5c2e388efd25957ac325850c1
Jinglin Zhao <jvz5625@comp-sc-0215.acib.production.int.aci.ics.psu.edu>
2022-04-11 15:07:59 -0400

    working on combining codes for paper

---
+++ .ipynb_checkpoints/functions-checkpoint.py
@@ -0,0 +1,122 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+def plot_all(k_mode, t, rv, erv, ind, eind, ts_xlabel, rv_xlabel, pe_xlabel, ind_yalbel, file_name, height_ratio=0.7, vlines=[], HARPS=False):
+
+	'''
+	e.g. 
+		k_mode 		= 11
+		t 			= bjd_daily
+		rv 			= rv_daily
+		erv 		= erv_daily
+		ind 		= ΔRV_k
+		eind 	 	= eRV_FT_k
+		ts_xlabel 	= 'BJD - 2400000 [d]'
+		rv_xlabel 	= '$RV_{HARPS}$'
+		pe_xlabel 	= 'Period [days]'
+		ind_yalbel	= 'A'
+		file_name 	= 'time-series_and_shift_correlation.png'
+
+	'''
+
+	def new_periodogram(x, y, dy, vlines, height_ratio=height_ratio, plot_min_t=2, max_f=1, spp=100):
+	
+		from scipy.signal import find_peaks
+		from astropy.timeseries import LombScargle
+
+		time_span = (max(x) - min(x))
+		min_f   = 1/time_span
+
+		frequency, power = LombScargle(x, y, dy).autopower(minimum_frequency=min_f,
+													   maximum_frequency=max_f,
+													   samples_per_peak=spp)
+
+		plot_x = 1/frequency
+		idxx = (plot_x>plot_min_t) & (plot_x<time_span/2)
+		height = max(power[idxx])*height_ratio
+		ax.plot(plot_x[idxx], power[idxx], 'k-', alpha=0.5)
+		peaks, _ = find_peaks(power[idxx], height=height)
+		ax.plot(plot_x[idxx][peaks], power[idxx][peaks], "ro")
+
+		for n in range(len(plot_x[idxx][peaks])):
+			ax.text(plot_x[idxx][peaks][n], power[idxx][peaks][n], '%.1f' % plot_x[idxx][peaks][n], fontsize=10)
+
+		ax.set_xlim([plot_min_t,time_span/2])
+		ax.set_ylim([0, 1.25*max(power[idxx])])
+
+		if vlines!=[]:        
+			for xc in vlines:
+				ax.axvline(x=xc, color='r', linestyle='-', lw=2, alpha=0.2)        
+		if HARPS == True:
+			ax.axvspan(200, 220, facecolor='r', alpha=0.2)
+
+		ax.set_xscale('log')
+
+
+	from sklearn.linear_model import LinearRegression
+
+	# set up the plotting configureations
+	alpha1, alpha2 = [0.5,0.2]
+	widths 	= [7,1,7]
+	heights = [1]*(k_mode+1)
+	gs_kw 	= dict(width_ratios=widths, height_ratios=heights)
+	plt.rcParams.update({'font.size': 12})
+	fig6, f6_axes = plt.subplots(figsize=(16, k_mode+1), ncols=3, nrows=k_mode+1, constrained_layout=True,
+	                             gridspec_kw=gs_kw)
+
+	# plots 
+	for r, row in enumerate(f6_axes):
+		for c, ax in enumerate(row):	
+
+			# time-series 
+			if c==0:
+				if r==0:
+					ax.errorbar(t, rv-np.mean(rv), erv, marker='.', ms=5, color='black', ls='none', alpha=alpha1)
+					ax.set_title('Time-series')
+					ax.set_ylabel(rv_xlabel)
+				else:				
+					ax.errorbar(t, ind[r-1,:], eind[r-1,:],  marker='.', ms=5, color='black', ls='none', alpha=alpha1)
+					ax.set_ylabel(ind_yalbel + '$_{' + str(r) + '}$')
+				if r!=k_mode:
+					ax.tick_params(labelbottom=False)
+				else:
+					ax.set_xlabel(ts_xlabel)
+
+			if c==1:
+				if r==0:
+					reg = LinearRegression().fit(rv.reshape(-1, 1), rv.reshape(-1, 1))
+					score = reg.score(rv.reshape(-1, 1), rv.reshape(-1, 1))
+					adjust_R2 = 1-(1-score)*(len(t)-1)/(len(t)-1-1)
+					title = r'$\bar{R}$' + r'$^2$'
+					ax.set_title(title + ' = {:.2f}'.format(adjust_R2))					
+					ax.plot(rv-np.mean(rv), rv-np.mean(rv), 'k.', alpha = alpha2)				
+				if r>0:
+					reg = LinearRegression().fit(rv.reshape(-1, 1), ind[r-1,:].reshape(-1, 1))
+					score = reg.score(rv.reshape(-1, 1), ind[r-1,:].reshape(-1, 1))
+					adjust_R2 = 1-(1-score)*(len(t)-1)/(len(t)-1-1)
+					title = r'$\bar{R}$' + r'$^2$'
+					ax.set_title(title + ' = {:.2f}'.format(adjust_R2))
+					ax.plot(rv-np.mean(rv), ind[r-1,:], 'k.', alpha = alpha2)
+				if r!=k_mode:
+					ax.tick_params(labelbottom=False)
+				else:
+					ax.set_xlabel(rv_xlabel)
+				ax.yaxis.tick_right()
+
+			if c==2:
+				if r==0:
+					new_periodogram(t, rv, erv, vlines)
+					ax.set_title('Periodogram')
+				if r>0:
+					new_periodogram(t, ind[r-1,:], eind[r-1,:], vlines)
+				if r!=k_mode:
+					ax.tick_params(labelbottom=False)
+				if r==k_mode:
+					ax.set_xlabel(pe_xlabel)
+
+	fig6.align_ylabels(f6_axes[:, 0])
+	plt.savefig(file_name)
+	plt.show()    
+	plt.close('all')
