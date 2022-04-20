110bc0c6a7a042bc54c89a2c2a0c62d9f3fec5a8
AƧTЯOƆAT <jinglinzhao@gmail.com>
2022-04-20 14:54:54 -0400

    Figure 11

--- .ipynb_checkpoints/functions-checkpoint.py
+++ .ipynb_checkpoints/functions-checkpoint.py
@@ -347,3 +347,242 @@ def long_short_divide(x, y, yerr, r):
 	short_term 	= y - y_pred
 
 	return gp, short_term, long_term
+
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+
+def lasso_lambda(X, Y, Weight, alphas, n_folds=5, N=100, title='', file_name=None):
+
+	from sklearn.linear_model import Lasso
+	from sklearn.model_selection import GridSearchCV
+	from sklearn.utils import shuffle
+	from datetime import datetime
+	from alive_progress import alive_bar
+
+	start_time  = datetime.now()
+
+	alpha_size 	= len(alphas)
+	array_size 	= len(Y)
+
+	alpna_n 		= np.zeros(N)
+	# coef 			= np.zeros((36,N))
+	scores_test 	= np.zeros((alpha_size, N))
+	scores_train 	= np.zeros((alpha_size, N))
+	scores_test_std = np.zeros((alpha_size, N))
+	scores_train_std= np.zeros((alpha_size, N))
+
+	tuned_parameters 	= [{"alpha": alphas}]
+
+	with alive_bar(N) as bar:
+
+		for n in range(N):
+			idx_random 	= shuffle(np.arange(array_size))
+			X 			= X[idx_random,:]
+			Y 			= Y[idx_random]
+			sw 			= Weight[idx_random]
+
+			lasso 		= Lasso()
+			clf 		= GridSearchCV(lasso, tuned_parameters, cv=n_folds, scoring='neg_root_mean_squared_error', refit=True, return_train_score=True)
+			clf.fit(X, Y, sample_weight=sw)
+			# coef[:,n] 	= clf.best_estimator_.coef_
+			alpna_n[n] 	= clf.best_estimator_.alpha
+
+			scores_test[:,n] 		= -clf.cv_results_["mean_test_score"]
+			scores_train[:,n] 		= -clf.cv_results_["mean_train_score"]
+			scores_test_std[:,n]	= clf.cv_results_["std_test_score"] / np.sqrt(n_folds)
+			scores_train_std[:,n] 	= clf.cv_results_["std_train_score"] / np.sqrt(n_folds)
+
+			bar()
+
+	scores_test_mean 		= np.mean(scores_test, axis=1)
+	scores_train_mean 		= np.mean(scores_train, axis=1)
+	scores_test_std_mean 	= np.mean(scores_test_std, axis=1)
+	scores_train_std_mean 	= np.mean(scores_train_std, axis=1)
+
+	end_time = datetime.now()
+	print('Duration: {}'.format(end_time - start_time))
+
+	# plots
+	if file_name != None:
+		plt.figure().set_size_inches(8, 6)
+		plt.rcParams['font.size'] = '16'
+		plt.gcf().subplots_adjust(left=0.15)
+
+		# plot error lines showing +/- std. errors of the scores
+		plt.title(title)
+		plt.semilogx(alphas, scores_test_mean, 'r', label='testing')
+		plt.semilogx(alphas, scores_train_mean, 'b', label='training')
+		plt.semilogx(alphas, scores_test_mean + scores_test_std_mean, "r--")
+		plt.semilogx(alphas, scores_test_mean - scores_test_std_mean, "r--")
+		plt.semilogx(alphas, scores_train_mean + scores_train_std_mean, "b--")
+		plt.semilogx(alphas, scores_train_mean - scores_train_std_mean, "b--")	
+		plt.fill_between(alphas, scores_test_mean + scores_test_std_mean, scores_test_mean - scores_test_std_mean, color='r', alpha=0.2)
+		plt.fill_between(alphas, scores_train_mean + scores_train_std_mean, scores_train_mean - scores_train_std_mean, color='b', alpha=0.2)
+		
+		xx 	= np.median(alpna_n)
+		plt.axvline(xx, color='k', alpha=0.5)
+		plt.text(xx, scores_test_mean[alphas==xx], r'$\hat\lambda_1$ = {:.3f}'.format(xx))
+
+		# determine a more robust lambda using the 1-sigma rule
+		scores_1sigma = (scores_test_mean+scores_test_std_mean)[alphas==xx][0]
+		for i in range(alpha_size-1):
+			if (scores_test_mean[i] < scores_1sigma) & (scores_test_mean[i+1] > scores_1sigma):
+				alpha_1sigma = alphas[i]
+		plt.axvline(alpha_1sigma, color='k', alpha=0.5)			
+		plt.axhline(scores_1sigma, linestyle="--", color=".5")
+		plt.text(alpha_1sigma, scores_1sigma, r'$\hat\lambda_2$ = {:.3f}'.format(alpha_1sigma))
+		plt.ylabel("Residual WRMS [m/s]")
+		plt.xlabel(r"$\lambda$")
+		plt.axhline(np.min(scores_test_mean), linestyle="--", color=".5")
+		plt.xlim([alphas[0], alphas[-1]])
+		plt.legend()
+		
+		plt.savefig(file_name + '.pdf')
+		plt.show()
+		plt.close()
+	
+	return scores_test_mean, scores_train_mean, scores_test_std_mean, scores_train_std_mean, alpna_n
+
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+
+def mlr(feature_matrix, target_vector, etarget_vector, alpha, lag='True', day=5, feature_matrix2=None):
+
+	'''
+		Multiple linear regression
+		no lag involved 
+	'''		
+
+	from sklearn import linear_model
+	from sklearn.linear_model import Lasso
+	
+	regr 	= linear_model.LinearRegression()
+	lasso 	= Lasso(alpha=alpha, max_iter=10000).fit(feature_matrix, target_vector, sample_weight=1/etarget_vector**2)
+
+	y_hat 			= lasso.predict(feature_matrix)
+	_, w_std_all 	= weighted_avg_and_std(target_vector, 1/etarget_vector**2)
+	_, res_wrms 	= weighted_avg_and_std((y_hat - target_vector), weights=1/etarget_vector**2)
+	_, model_wrms 	= weighted_avg_and_std(y_hat, weights=1/etarget_vector**2)
+	score 			= lasso.score(feature_matrix, target_vector, sample_weight=1/etarget_vector**2)
+	n, p 			= len(target_vector), feature_matrix.shape[1]
+	adjust_R2 		= 1-(1-score)*(n-1)/(n-p-1)
+	score 			= adjust_R2
+
+	if lag=='False':
+		k_feature2 	= feature_matrix.shape[1]
+	else:
+		k_feature2 	= int(feature_matrix.shape[1]/(day+1))
+		k_feature 	= int(k_feature2/2)
+
+	w_std = np.zeros(k_feature2)
+	for i in range(len(w_std)):
+		if lag=='True':
+			_, w_std[i] = weighted_avg_and_std(feature_matrix2[:,i], 1/etarget_vector**2)
+		else:
+			_, w_std[i] = weighted_avg_and_std(feature_matrix[:,i], 1/etarget_vector**2)
+	print('Weighted rms is reduced from {:.2f} to {:.2f} (-{:.0f}%); \n\
+		Modelled RV weigthed rms = {:.2f};\n\
+		Adjusted R squared = {:.3f}.'
+			.format(w_std_all, res_wrms, (1-res_wrms/w_std_all)*100, model_wrms, score))
+
+	if lag=='False':
+
+		import pandas as pd 
+		df = pd.DataFrame({	'coefficients'		: lasso.coef_, 
+							'std'				: w_std,
+							'variance'			: (lasso.coef_*w_std)**2,
+							'variance percentage': (lasso.coef_*w_std)**2 / sum((lasso.coef_*w_std)**2) * 100
+							})	
+		print(df.round({'coefficients'	: 2, 
+						'std'		: 2,
+						'variance'		: 2,
+						'variance percentage': 1
+						}))
+		
+		return y_hat, w_std_all, res_wrms, score, df
+
+	else: # maybe problematic!!!
+
+		coeff_matrix = np.zeros((day*2+1, k_feature2))
+		for i in range(k_feature):
+			coeff_matrix[:,i] = lasso.coef_[i:k_feature*(2*day+1):k_feature]
+		coeff_matrix[day,k_feature:]=lasso.coef_[k_feature*(2*day+1):]
+
+		variance_matrix = np.zeros(coeff_matrix.shape)
+		for i in range(day*2+1):
+			variance_matrix[i,:] = coeff_matrix[i,:]*w_std
+		variance_matrix = variance_matrix**2
+		variance_matrix = variance_matrix / variance_matrix.sum() * 100
+
+		return y_hat, w_std_all, res_wrms, score, variance_matrix
+    
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+
+def weighted_avg_and_std(values, weights):
+    """
+    Return the weighted average and standard deviation.
+
+    values, weights -- Numpy ndarrays with the same shape.
+    """
+    average = np.average(values, weights=weights)
+    variance = np.average((values-average)**2, weights=weights)
+    
+    return (average, np.sqrt(variance))
+
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+
+def imshow_matrix(coeff_array, score, res_wrms, alpha, k_max, file_name):
+
+	from matplotlib import colors
+
+	'''
+		file_name = fwhm_bis_coef
+		file_name = fwhm_bis_multi_coef
+		file_name = pca_coef
+	'''
+
+	day = int((coeff_array.shape[0]-1)/2)
+	x = np.arange(day * 2 + 1) - day
+	y = np.arange(coeff_array.shape[1]) + 1
+
+	from mpl_toolkits.axes_grid1 import make_axes_locatable
+	fig = plt.figure(figsize=(len(x)/1.5+2, len(y)/1.5+1), frameon=False)
+	title = r'$\bar{R}$' + r'$^2$'
+	plt.title(title + ' = {:.3f}'.format(score)
+			  + '   Residual WRMS = {:.2f} m/s'.format(res_wrms))
+	plt.xlabel('Lag [days]')
+	ax = plt.gca()
+
+	if file_name == 'lasso_coef':
+		im2 = plt.imshow(np.transpose(coeff_array),  vmin=-5, vmax=5, cmap=plt.cm.bwr, alpha=.9)
+	else:
+		im2 = plt.imshow(np.transpose(coeff_array),  cmap=plt.cm.get_cmap('Reds'), alpha=.9, norm=colors.LogNorm(vmin=0.1, vmax=100))
+
+	# Loop over data dimensions and create text annotations.
+	for i in range(coeff_array.shape[1]):
+		for j in range(day*2+1):
+			if coeff_array[j, i] != 0:
+				text = ax.text(j, i, '{:.1f}'.format(coeff_array[j, i]),
+							   ha="center", va="center", color="k")
+
+	ax.set_xticks(np.arange(len(x)))
+	ax.set_yticks(np.arange(len(y)))
+	ax.set_xticklabels(x)
+	if file_name == 'fwhm_bis_coef':
+		ax.set_yticklabels(['FWHM', 'BIS'])
+	if file_name == 'fwhm_bis_multi_coef':
+		ax.set_yticklabels(['S-FWHM', 'S-BIS', 'L-FWHM', 'L-BIS'])
+	if file_name == 'pca_coef':	
+		ax.set_yticklabels([r'PC$_1$', r'PC$_2$', r'PC$_3$'])	
+	if file_name == 'fiesta_multi_coef':
+		ax.set_yticklabels([r'S-PC$_1$', r'S-PC$_2$', r'S-PC$_3$', r'L-PC$_1$', r'L-PC$_2$', r'L-PC$_3$'])
+
+	divider = make_axes_locatable(ax)
+	cax = divider.append_axes("right", size="5%", pad=0.05)
+
+	plt.colorbar(im2, cax=cax, label='Variance percentage') 
+	plt.savefig(file_name + '_{:.2f}_{:d}_{:d}'.format(alpha, day, k_max) +'.pdf')
+	plt.show()
+	plt.close()
